0a8b4f59c694b1540d079ea0316c5345de9c6afb
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2023-01-27 16:49:34 +0900

    update(css)

--- js/background.js
+++ js/background.js
@@ -7,3 +7,9 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+bgImage.style.position = "absolute";
+bgImage.style.top = "0";
+bgImage.style.width = "100%";
+bgImage.style.height = "100vh";
+bgImage.style.zIndex = "-1";
